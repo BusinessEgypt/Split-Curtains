e2181e7b1ee0f8c8e9fbc4c37672e9b8537cef3f
Ahmed <eng.abadr89@gmail.com>
2025-06-11 10:43:40 +0300

    feat: unified PO creation on invoice post and payment update in account.move

--- split_curtains_base/models/account_move.py
+++ split_curtains_base/models/account_move.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from odoo import models, fields, api, _
 import logging
 
@@ -8,8 +9,23 @@ class AccountMove(models.Model):
 
     def action_post(self):
         res = super(AccountMove, self).action_post()
+        # نشغل إنشاء الـ PO عند ترحيل الفاتورة لو الدفع تم
         for inv in self.filtered(lambda m: m.move_type == 'out_invoice' and m.payment_state == 'paid'):
             self._create_po_after_payment(inv)
+        return res
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        # هنا بنراقب لو فيه فاتورة جديدة متولدة
+        moves = super(AccountMove, self).create(vals_list)
+        for inv in moves.filtered(lambda m: m.move_type == 'out_invoice' and m.payment_state == 'paid'):
+            self._create_po_after_payment(inv)
+        return moves
+
+    def write(self, vals):
+        res = super(AccountMove, self).write(vals)
+        if 'payment_state' in vals and vals['payment_state'] == 'paid':
+            self._create_po_after_payment(self)
         return res
 
     def _create_po_after_payment(self, invoice):
@@ -17,12 +33,8 @@ class AccountMove(models.Model):
         if not sale_order or sale_order.x_po_created_from_invoice:
             return
 
-        lines = sale_order.order_line.filtered(lambda l: l.product_id.type == 'product')
-        if not lines:
-            return
-
         created_purchase_orders = self.env['purchase.order']
-        for line in lines:
+        for line in sale_order.order_line.filtered(lambda l: l.product_id.type == 'product'):
             try:
                 moves = line._action_launch_stock_rule()
                 for move in moves:
@@ -37,4 +49,4 @@ class AccountMove(models.Model):
             for po in created_purchase_orders:
                 if po.state == 'draft':
                     po.button_confirm()
-                po.message_post(body=f'🧰 Auto-created PO from SO {sale_order.name} via Invoice {invoice.name}')
+                po.message_post(body=f'🧰 Auto-created PO from SO {sale_order.name} via Invoice/Payment {invoice.name}')
